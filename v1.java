5c13dab219776c88bb11f8e6148d90b111b5aa82
tay_ma_alda <temyhey@gmail.com>
2024-06-13 19:47:05 +0200

    changes the number input to a string + subtraction op: done

--- v1.java
+++ v1.java
@@ -1,26 +1,37 @@
 import java.util.Scanner;
 
 public class v1 {
-    static int operation;
+    static String operation;
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("What operation do you wish to do? (1 = addition)");
-        operation = scanner.nextInt();
-        if (operation == 1) {
-            System.out.println("please enter the numbers you would like to add (first number, second number)");
+        System.out.println("What operation do you wish to do?");
+        operation = scanner.nextLine();
+        if (operation.equals("addition")) {
+            System.out.println(
+                    "please enter the numbers you would like to add (first number, 'enter key', second number)");
             int first_num = scanner.nextInt();
             int second_num = scanner.nextInt();
             int result = addition(first_num, second_num);
             System.out.println("the result: " + result);
-        } else if (operation == 2) {
-
+        } else if (operation.equals("subtraction")) {
+            System.out.println(
+                    "please enter the number you would like to subtracte (first number, 'enter key', second number)");
+            int first_num = scanner.nextInt();
+            int second_num = scanner.nextInt();
+            int result = subtraction(first_num, second_num);
+            System.out.println("the result: " + result);
         }
     }
 
     public static int addition(int first_num, int second_num) {
         int result = first_num + second_num;
+        return result;
+    }
+
+    public static int subtraction(int first_num, int second_num) {
+        int result = first_num - second_num;
         return result;
     }
 }
