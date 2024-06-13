b11520eb2c42a5351edd4d1d3df5ca0db9ced487
tay_ma_alda <temyhey@gmail.com>
2024-06-13 19:40:51 +0200

    addition op: done

--- v1.java
+++ v1.java
@@ -1,20 +1,22 @@
 import java.util.Scanner;
 
 public class v1 {
-    static String operation = "";
+    static int operation;
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("What operation do you wish to do?");
-        operation = scanner.next();
-        if (operation == "addition") {
+        System.out.println("What operation do you wish to do? (1 = addition)");
+        operation = scanner.nextInt();
+        if (operation == 1) {
             System.out.println("please enter the numbers you would like to add (first number, second number)");
             int first_num = scanner.nextInt();
             int second_num = scanner.nextInt();
-            addition(first_num, second_num);
-        }
+            int result = addition(first_num, second_num);
+            System.out.println("the result: " + result);
+        } else if (operation == 2) {
 
+        }
     }
 
     public static int addition(int first_num, int second_num) {
