95f456296e81f93d3ecd005f90c0368eb6581c6f
tay_ma_alda <temyhey@gmail.com>
2024-06-13 14:03:55 +0200

    the only thing that works is the scanner of the first question ;-; nothing else works

--- v1.java
+++ v1.java
@@ -1,3 +1,24 @@
+import java.util.Scanner;
+
 public class v1 {
-    // test
+    static String operation = "";
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("What operation do you wish to do?");
+        operation = scanner.next();
+        if (operation == "addition") {
+            System.out.println("please enter the numbers you would like to add (first number, second number)");
+            int first_num = scanner.nextInt();
+            int second_num = scanner.nextInt();
+            addition(first_num, second_num);
+        }
+
+    }
+
+    public static int addition(int first_num, int second_num) {
+        int result = first_num + second_num;
+        return result;
+    }
 }
