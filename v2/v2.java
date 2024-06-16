cd50b02cb25aa27178f419a4ce861fc79bdce53d
tay_ma_alda <temyhey@gmail.com>
2024-06-16 16:55:42 +0200

    i tried to come and create the actions (at least the +) but there's a problem: JTextFields are a whole input, like int and String. so i need to fix that to be able to make calculations. (there's a link in the v2 file to a stackoverflow solution for this)

--- v2/v2.java
+++ v2/v2.java
@@ -14,8 +14,33 @@ public class v2 {
         frame_v2 frame = new frame_v2();
         frame.add(label);
 
+        if(frame.op.equals("+")) {
+            // addition(frame.fstNum, frame.sndNum); // look up here: "https://stackoverflow.com/questions/1313390/is-there-any-way-to-accept-only-numeric-values-in-a-jtextfield" to fix the JTextField input problem
+        }
+
 
 
         
     }
+
+    public int addition(int first_num, int second_num) {
+        int result = first_num + second_num;
+        return result;
+    }
+
+    public static int subtraction(int first_num, int second_num) {
+        int result = first_num - second_num;
+        return result;
+    }
+
+    public static int multiplication(int first_num, int second_num) {
+        int result = first_num * second_num;
+        return result;
+    }
+
+    public static double division(int first_num, int second_num) {
+        double result = first_num / second_num;
+        return result;
+    }
+
 }
