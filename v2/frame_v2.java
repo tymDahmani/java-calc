8a18ce124263b708eaf61c08e5258b4dda9e521e
tay_ma_alda <temyhey@gmail.com>
2024-06-16 16:16:10 +0200

    well, the frame wasn't ready yet, as expected. added txt boxes + cool img + button. been tough but this is a break commit. TODO: add frames to the txtBoxes + store their input. then do the actions for each op

--- v2/frame_v2.java
+++ v2/frame_v2.java
@@ -1,22 +1,69 @@
 
 import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JTextField;
 
-public class frame_v2 extends JFrame {
+public class frame_v2 extends JFrame implements ActionListener {
+
+    JButton calculate;
+    JTextField fstNum;
+    JTextField sndNum;
+    JTextField op;
 
     frame_v2() {
         
+        calculate = new JButton();
+        calculate.setBounds(162, 390, 95, 40);
+        calculate.addActionListener(this);
+        calculate.setText("calculate");
+        calculate.setFocusable(false);
+        calculate.setFont(new Font("Times New Roman", Font.BOLD,15));
+        calculate.setForeground(Color.white);
+        calculate.setBackground(new Color(54, 6, 89));
+        calculate.setBorder(BorderFactory.createEtchedBorder());
+        // method for making the button disabled before inputting the numbers to calculate: `calculate.setEnable(false);`
+        // or, you can put this same like in the actionPerformed method to make it enabled only once (after inputting the numbers)
+
         this.setTitle("java-calc v2");
         ImageIcon icon = new ImageIcon("v2/other/sqrt(-1).png");
         this.setIconImage(icon.getImage());
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+        fstNum =  new JTextField();
+        fstNum.setPreferredSize(new Dimension(100, 40));
+        fstNum.setBounds(117, 300, 50, 43);
+
+        sndNum = new JTextField();
+        sndNum.setPreferredSize(new Dimension(100, 40));
+        sndNum.setBounds(250, 300, 50, 43);
+
+        op = new JTextField();
+        op.setPreferredSize(new Dimension(100, 40));
+        op.setBounds(189, 300, 40, 33);
+
         this.setResizable(false);
-        this.setSize(420, 420);
+        this.setSize(420, 620);
         this.getContentPane().setBackground(Color.black); //rgb (the order of costume colors) usage (new Color(r,g,b)\or\)
         this.setVisible(true);
+        this.add(calculate);
+        this.add(fstNum);
+        this.add(sndNum);
+        this.add(op);
 
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(e.getSource()==calculate) {
+            System.out.println("yo, i should be calculating");
+        }
     }
     
 }
--- v2/v2.java
+++ v2/v2.java
@@ -1,8 +1,21 @@
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
 
 public class v2 {
     public static void main(String[] args) {
         
-        new frame_v2();
+        ImageIcon icon = new ImageIcon("v2/other/mcskullwatchingyou(klein).png");
+        
+
+        JLabel label = new JLabel();
+        label.setIcon(icon);
+        label.setVerticalAlignment(JLabel.TOP);
+
+        frame_v2 frame = new frame_v2();
+        frame.add(label);
+
+
+
         
     }
 }
