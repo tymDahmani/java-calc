4438a74732ed9be7adcef38191512bf7cbeeb59f
tay_ma_alda <temyhey@gmail.com>
2024-07-02 17:17:39 +0200

    just so i wont forget to commit: this code has the operators if statements, they should've worked but they didnt. the problem is after the print statement of \"enter the operation name or symbol\", it just breaks and ends the program before i evne enter my input!

--- v3/division.java
+++ v3/division.java
@@ -1,12 +1,13 @@
 package v3;
 
 public class division {
-    void division(int a, int b) {
+    double division(double a, double b) {
         double c = a / b;
         double d = a % b;
         if (d != 0) {
             System.out.println("remainder: " + d);
         }
-        System.out.println("quotient: " + c);
+        double quotient = c;
+        return quotient;
     }
 }
--- v3/v3.java
+++ v3/v3.java
@@ -7,24 +7,32 @@ public class v3 {
 
         addition addition = new addition();
         subtraction subtraction = new subtraction();
+        multiplication multiplication = new multiplication();
+        division division = new division();
         Scanner sc = new Scanner(System.in);
 
         // initializing the inputs
+        System.out.println("please enter the numbers you would like to operate on: ");
         double a = sc.nextDouble(); // this shit seems to only work with commas as double/float numbers - dots don't work
         double b = sc.nextDouble();
 
+        System.out.println(
+            "enter the operation name or its symbol: "
+        );
+        String operator = sc.nextLine(); 
         // main if statement:
-        if (sc.nextLine().equalsIgnoreCase("addition") || sc.nextLine().equalsIgnoreCase("+")) {
-            double result = addition.addtion(a, b);
-            System.out.println("sum: " + result);
-        } else if (sc.nextLine().equalsIgnoreCase("subtraction") || sc.nextLine().equalsIgnoreCase("-")) {
-            double result = subtraction.subtraction(a, b);
-            System.out.println("subtraction: " + result);
-        } else if (sc.nextLine().equalsIgnoreCase("multiplication") || sc.nextLine().equalsIgnoreCase("*")) {
-            // hi ?
+        if (operator.equalsIgnoreCase("addition") || operator.equals("+")) {
+            double sum = addition.addtion(a, b);
+            System.out.println("sum: " + sum);
+        } else if (operator.equalsIgnoreCase("subtraction") || operator.equals("-")) {
+            double difference = subtraction.subtraction(a, b);
+            System.out.println("difference: " + difference);
+        } else if (operator.equalsIgnoreCase("multiplication") || operator.equals("*")) {
+            double product = multiplication.multiplication(a, b);
+            System.out.println("product: " + product);
+        } else if (operator.equalsIgnoreCase("division") || operator.equals("/")) {
+            double quotient = division.division(a, b);
+            System.out.println("quotient: " + quotient);
         }
-
-        double sum = addition.addtion(a, b);
-        System.out.println(sum);
     }
 }
